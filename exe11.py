ac2a689037e4eda6c1f71ceff53141a503751ca6
sudharshanbv <bvsudharshan@gmail.com>
2018-02-13 19:15:52 +0530

    second commit

---
+++ exe11.py
@@ -0,0 +1,21 @@
+
+
+"""
+1	print("How	old	are	you?",	end='	')
+2	age	=	input()
+3	print("How	tall	are	you?",	end='	')
+4	height	=	input()
+5	print("How	much	do	you	weigh?",	end='	')
+6	weight	=	input()
+7
+8	print(f"So,	you're	{age}	old,	{height}	tall	and	{weight}	heavy.")
+"""
+
+
+print ("How old are you?", end=' ')
+age = input()
+
+print ("How tall are you?", end=' ')
+tall = input()
+
+print (f"So, you are {age} old and {tall} tall")
